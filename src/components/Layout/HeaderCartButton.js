2dc1e18e7faa71ab20402616e507f247521f23ba
Peter Nguyen <ptn045@ucsd.edu>
2023-09-07 12:37:29 -0700

    add animation to HeaderCartButton with useEfffect

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,33 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon.js';
 import CartContext from '../../store/cart-context.js';
 import styles from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+	const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
 	const cartCtx = useContext(CartContext);
-	const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+	const { items } = cartCtx;
+	const numberOfCartItems = items.reduce((curNumber, item) => {
 		return curNumber + item.amount;
 	}, 0);
 
-	const btnClasses = `${styles.button} ${styles.bump}`;
+	const btnClasses = `${styles.button} ${btnIsHighlighted ? styles.bump : ''}`;
+
+	useEffect(() => {
+		if (items.length === 0) {
+			return;
+		}
+		setBtnIsHighlighted(true);
+
+		const timer = setTimeout(() => {
+			setBtnIsHighlighted(false);
+		}, 300);
+
+		// Cleanup function
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [items]);
 
 	return (
 		<button className={btnClasses} onClick={props.onClick}>
